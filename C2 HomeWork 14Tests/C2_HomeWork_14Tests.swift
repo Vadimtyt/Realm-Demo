adb92d83f5b6328d3048e691ddacdf95f4c00107
Vadim <vadimtyt@mail.ru>
2020-10-29 23:52:26 +0300

    Initial Commit

---
+++ C2 HomeWork 14Tests/C2_HomeWork_14Tests.swift
@@ -0,0 +1,33 @@
+//
+//  C2_HomeWork_14Tests.swift
+//  C2 HomeWork 14Tests
+//
+//  Created by Вадим on 29.10.2020.
+//
+
+import XCTest
+@testable import C2_HomeWork_14
+
+class C2_HomeWork_14Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
